1328abcb387dde0e8c1f3150005f723af62d31b9
mj268 <markknpr685@gmail.com>
2019-05-03 09:06:29 +0100

    Quickstart up to dynamic routing

--- flaskTest.py
+++ flaskTest.py
@@ -1,7 +1,29 @@
+# flaskTest.py - playing around with Flask
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Index Page"
+
 @app.route("/hello")
 def hello():
     return "Hello World!"
+
+# Example routes using variable names to make the routes dynamic
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
